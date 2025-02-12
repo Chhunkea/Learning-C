44ad1796fe678f0ab13b7cc9964229176b15da6b
Chhunkea <kimchhunkea64@gmail.com>
2025-02-12 18:41:15 +0700

    Feat: add datatype of C

---
+++ datatype.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Datatype in C
+int main() {
+  int age = 10;
+  double gpa = 3.6;
+  char grade ='A';
+  char phrase[] = "Giraffe";
+
+  printf("%d, %f, %c, %s", age, gpa, grade, phrase);
+}
