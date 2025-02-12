d6f19582a97f2c2fd44e91e06eb366028a87b7e3
Chhunkea <chhunkea@Kims-MacBook-Pro.local>
2025-02-12 18:36:45 +0700

    Feat: add c main programming

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// %s for strings
+// %d for integers
+// %f for floating-point numbers
+// %c for single characters
+
+// Variable
+int main() {
+  char Name[] = "Chhunkea";
+  int Age = 67;
+  printf("My name is %s and my age is %d\n",Name, Age);
+}
